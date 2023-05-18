132b4d0120080c64909717509bda32dba0bb219f
jatu-studiobox <tj_yai@hotmail.com>
2023-05-19 01:32:29 +0700

    Test line notify

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
+  const nameToGreet = core.getInput("who-to-greet");
   console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
+  const time = new Date().toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
+
+  const token = core.getInput("access_token");
+  const repositoryName = core.getInput("repository_name");
+  const issueNo = core.getInput("issue_number");
+  const issueTitle = core.getInput("issue_title");
+  const issueHtmlUrl = core.getInput("issue_html_url");
+
+  let message = "\nIssue is opened.";
+  message += "\n📦: " + repositoryName;
+  message += "\n[#" + issueNo + "]: " + issueTitle;
+  message += "\n" + issueHtmlUrl;
+
+  const formLineNotify = new FormData();
+
+  formLineNotify.append("message", message);
+
+  let lineConfig = {
+    url: "https://notify-api.line.me/api/notify",
+    method: "POST",
+    headers: {
+      "Content-Type": "multipart/form-data",
+      Authorization: "Bearer " + token,
+      ...formLineNotify.getHeaders(),
+    },
+    data: formLineNotify,
+  };
+
+  axios(lineConfig)
+    .then((res) => {
+      const data = res.data;
+      console.log(data);
+      if (data.status !== 200) {
+        core.setFailed(data.message);
+      }
+    })
+    .catch((error) => {
+      core.setFailed(error.message);
+    });
+
 } catch (error) {
   core.setFailed(error.message);
 }
