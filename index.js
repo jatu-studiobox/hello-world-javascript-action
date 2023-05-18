e75ca961ff3000b39c3c1a433da05e7f28b6c9ee
jatu-studiobox <tj_yai@hotmail.com>
2023-05-19 01:44:37 +0700

    Fix LINE Notify

--- index.js
+++ index.js
@@ -4,18 +4,21 @@ const github = require("@actions/github");
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput("who-to-greet");
+  const token = core.getInput("access_token");
+  const repositoryName = core.getInput("repository_name");
+  const issueNo = core.getInput("issue_number");
+  const issueTitle = core.getInput("issue_title");
+  const issueHtmlUrl = core.getInput("issue_html_url");
+
   console.log(`Hello ${nameToGreet}!`);
+  
   const time = new Date().toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 
-  const token = core.getInput("access_token");
-  const repositoryName = core.getInput("repository_name");
-  const issueNo = core.getInput("issue_number");
-  const issueTitle = core.getInput("issue_title");
-  const issueHtmlUrl = core.getInput("issue_html_url");
+  
 
   let message = "\nIssue is opened.";
   message += "\n📦: " + repositoryName;
