8c71adefd6ade8b68ffa8a396ecd0b5b15f35210
jatu-studiobox <tj_yai@hotmail.com>
2023-05-19 04:21:38 +0700

    Test fix line notify

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const github = require("@actions/github");
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput("who-to-greet");
-  const token = core.getInput("access-token");
+  // const token = core.getInput("access-token");
   const repositoryName = core.getInput("repository-name");
-  const issueNo = core.getInput("issue-number");
-  const issueTitle = core.getInput("issue-title");
-  const issueHtmlUrl = core.getInput("issue-html-url");
+  // const issueNo = core.getInput("issue-number");
+  // const issueTitle = core.getInput("issue-title");
+  // const issueHtmlUrl = core.getInput("issue-html-url");
 
   console.log(`Hello ${nameToGreet}!`);
+  console.log(`repositoryName: ${repositoryName}!`);
 
   const time = new Date().toTimeString();
   core.setOutput("time", time);
@@ -20,35 +21,36 @@ try {
 
   let message = "\nIssue is opened.";
   message += "\n📦: " + repositoryName;
-  message += "\n[#" + issueNo + "]: " + issueTitle;
-  message += "\n" + issueHtmlUrl;
+  console.log(`message: ${message}!`);
+  // message += "\n[#" + issueNo + "]: " + issueTitle;
+  // message += "\n" + issueHtmlUrl;
 
-  const formLineNotify = new FormData();
+  // const formLineNotify = new FormData();
 
-  formLineNotify.append("message", message);
+  // formLineNotify.append("message", message);
 
-  let lineConfig = {
-    url: "https://notify-api.line.me/api/notify",
-    method: "POST",
-    headers: {
-      "Content-Type": "multipart/form-data",
-      Authorization: "Bearer " + token,
-      ...formLineNotify.getHeaders(),
-    },
-    data: formLineNotify,
-  };
+  // let lineConfig = {
+  //   url: "https://notify-api.line.me/api/notify",
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "multipart/form-data",
+  //     Authorization: "Bearer " + token,
+  //     ...formLineNotify.getHeaders(),
+  //   },
+  //   data: formLineNotify,
+  // };
 
-  axios(lineConfig)
-    .then((res) => {
-      const data = res.data;
-      console.log(data);
-      if (data.status !== 200) {
-        core.setFailed(data.message);
-      }
-    })
-    .catch((error) => {
-      core.setFailed(error.message);
-    });
+  // axios(lineConfig)
+  //   .then((res) => {
+  //     const data = res.data;
+  //     console.log(data);
+  //     if (data.status !== 200) {
+  //       core.setFailed(data.message);
+  //     }
+  //   })
+  //   .catch((error) => {
+  //     core.setFailed(error.message);
+  //   });
 
 } catch (error) {
   core.setFailed(error.message);
